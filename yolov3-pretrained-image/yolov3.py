8c261bc7a3c71cc34c570230c4b47f129cbf2b62
SelenNB <79100203+selennazlib@users.noreply.github.com>
2022-10-10 13:00:54 +0300

    Non - Maximum Suppression

--- yolov3-pretrained-image/yolov3.py
+++ yolov3-pretrained-image/yolov3.py
@@ -8,7 +8,7 @@ This is a temporary script file.
 import cv2
 import numpy as np
 
-path = 'yolov3-pretrained-image/images/img.jpg'
+path = 'yolov3-pretrained-image/images/cat.png'
 
 img = cv2.imread(path)
 # print(img) to crosscheck to see if we read the image right
@@ -76,6 +76,11 @@ model.setInput(img_blob)
 
 detection_layers = model.forward(output_layer)
 
+# non maximum suppression
+id_list = []
+boxes_list = []
+confidence_list = []
+
 
 for detection_layer in detection_layers:
     for object_detection in detection_layer:
@@ -84,7 +89,7 @@ for detection_layer in detection_layers:
         predicted_id = np.argmax(scores)
         confidence = scores[predicted_id]
         
-        if confidence > 0.7:
+        if confidence > 0.4:
             label = labels[predicted_id]
             bounding_box = object_detection[0:4] * np.array([img_width, img_height, img_width, img_height])
             (box_center_x ,box_center_y, box_width, box_height) = bounding_box.astype('int')
@@ -92,15 +97,35 @@ for detection_layer in detection_layers:
             start_x = int(box_center_x - (box_width / 2))
             start_y = int(box_center_y - (box_height / 2))
             
-            end_x = int(start_x + box_width)
-            end_y = int(start_y + box_height)
-            
-            box_color = colors[predicted_id]
-            box_color = [int(each) for each in box_color]
-            
-            cv2.rectangle(img, (start_x, start_y), (end_x, end_y), box_color, 1)
-            cv2.putText(img, label, (start_x, start_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
+            # non maximum suppression
+            id_list.append(predicted_id)
+            boxes_list.append([start_x, start_y, int(box_width), int(box_height)])
+            confidence_list.append(float(confidence))
 
+# non maximum suppression            
+max_ids = cv2.dnn.NMSBoxes(boxes_list, confidence_list, 0.5, 0.4)
+
+max_class_id = max_ids[0]
+box = boxes_list[max_class_id]
+
+start_x = box[0]
+start_y = box[1]
+box_width = box[2]
+box_height =  box[3]
+
+predicted_id = id_list[max_class_id]
+label = labels[predicted_id]
+confidence = confidence_list[max_class_id]
+
+
+end_x = int(start_x + box_width)
+end_y = int(start_y + box_height)
+
+box_color = colors[predicted_id]
+box_color = [int(each) for each in box_color]
+
+cv2.rectangle(img, (start_x, start_y), (end_x, end_y), box_color, 1)
+cv2.putText(img, label, (start_x, start_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, box_color, 1)
 
  
 cv2.imshow('Detection Windows', img)
